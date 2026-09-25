b816f46f06da11679db5cf8c1bb86a3ac3316237
s-dagatti <sdagatti@conci.com.ar>
2026-09-25 15:38:09 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -178,18 +178,18 @@ with tab_autotrac:
     )
 
     max_fecha_kpi = (
-        df_filtrado_autotrac["Fecha_fin_dt"].max()
-        if not df_filtrado_autotrac.empty
-        else None
+    df_filtrado_autotrac["Fecha_fin_dt"].max()
+    if not df_filtrado_autotrac.empty
+    else None
     )
-    inicio_ult_semana_kpi = (
-        max_fecha_kpi - pd.Timedelta(days=7) if pd.notna(max_fecha_kpi) else None
-    )
-
-    if inicio_ult_semana_kpi is not None:
+    
+    if pd.notna(max_fecha_kpi):
         df_kpi_ult_semana = df_filtrado_autotrac[
-            df_filtrado_autotrac["Fecha_fin_dt"] >= inicio_ult_semana_kpi
+            df_filtrado_autotrac["Fecha_fin_dt"] == max_fecha_kpi
         ]
+    else:
+        df_kpi_ult_semana = pd.DataFrame()
+        
         promedio_ult_semana_kpi = (
             df_kpi_ult_semana["AutoTrac™ Activo"].mean()
             if not df_kpi_ult_semana.empty
